1246908852c71a89d65590c671b38d2deaabf9b1
fahmida-faiza <fahmidafaiza57@gmail.com>
2025-05-27 23:37:27 +0600

    added Thunk

--- src/redux/features/user/userSlice.js
+++ src/redux/features/user/userSlice.js
@@ -1,9 +1,34 @@
-import { createSlice } from '@reduxjs/toolkit';
-
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import auth from '../../../utils/firebase.config'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
 const initialState = {
-  name: 'Mir Hussain',
-  email: 'mir@gmail.com',
+  // name: 'Mir Hussain',
+  // email: 'mir@gmail.com',
+  name: '',
+  email: '',
+  isLoading:true,
+  isError:false,
+  error:'',
 };
+
+// user create korbo
+export const createUser= createAsyncThunk(
+  'userSlice/createUser', 
+  async ({email, password}) => {
+  const data = await createUserWithEmailAndPassword(auth , email, password)
+console.log(data)
+  return;
+}
+);
+
+
+
+
+
+
+
+
+
 
 const userSlice = createSlice({
   name: 'userSlice',
