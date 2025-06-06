5ec95229fabbbc3b4e8cd370fab76f99c249237d
fahmida-faiza <fahmidafaiza57@gmail.com>
2025-06-06 23:14:15 +0600

    added api

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,15 +1,24 @@
 import { useEffect, useState } from 'react';
 import loginImage from '../assets/image/login.svg';
 import { useForm, useWatch } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import {  useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import {createUser} from '../redux/features/user/userSlice'
+import toast, {Toaster} from 'react-hot-toast';
 const Signup = () => {
   const { handleSubmit, register, control } = useForm();
   const password = useWatch({ control, name: 'password' });
   const confirmPassword = useWatch({ control, name: 'confirmPassword' });
   const navigate = useNavigate();
   const [disabled, setDisabled] = useState(true);
+
+
+const { isLoading, isError, error, email } = useSelector(
+  (state) => state.userSlice
+);
+
+
+
 const dispatch = useDispatch()
   useEffect(() => {
     if (
@@ -24,6 +33,23 @@ const dispatch = useDispatch()
       setDisabled(true);
     }
   }, [password, confirmPassword]);
+
+  useEffect(() => {
+    if(isError && error) {
+      toast.error(error);
+
+    }
+
+  }, [isError, error]);
+
+  useEffect(() => {
+    if(!isLoading && email) {
+     navigate('/')
+
+    }
+
+  }, [isLoading, email])
+
 
   const onSubmit = ({ name, email, password }) => {
     // Email Password signup
@@ -41,6 +67,7 @@ const dispatch = useDispatch()
 
   return (
     <div className="flex max-w-7xl mx-auto h-screen items-center">
+      <Toaster/>
       <div className="w-1/2">
         <img src={loginImage} className="h-full w-full" alt="" />
       </div>
@@ -89,6 +116,7 @@ const dispatch = useDispatch()
                 type="submit"
                 className="btn btn-primary w-full disabled:bg-gray-300 disabled:cursor-not-allowed"
                 disabled={disabled}
+                
               >
                 Sign up
               </button>
